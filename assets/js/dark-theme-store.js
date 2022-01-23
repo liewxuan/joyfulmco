3440df643531ba52ca6969fed67adf41e90f71d6
cycool29 <cycool29@gmail.com>
2022-01-23 21:19:00 +0800

    Update

--- assets/js/dark-theme-store.js
+++ assets/js/dark-theme-store.js
@@ -9,18 +9,35 @@ if (currentTheme == "dark") {
     document.body.classList.add("dark-theme");
 }
 
-// Listen for a click on the button 
-btn.addEventListener("click", function() {
-    // Toggle the .dark-theme class on each click
-    document.body.classList.toggle("dark-theme");
+// // Listen for a click for pc 
+// btn.addEventListener("click", function() {
+//     // Toggle the .dark-theme class on each click
+//     document.body.classList.toggle("dark-theme");
 
-    // Let's say the theme is equal to light
-    let theme = "light";
-    // If the body contains the .dark-theme class...
-    if (document.body.classList.contains("dark-theme")) {
-        // ...then let's make the theme dark
-        theme = "dark";
-    };
-    // Then save the choice in localStorage
-    localStorage.setItem("theme", theme);
-});
+//     // Let's say the theme is equal to light
+//     let theme = "light";
+//     // If the body contains the .dark-theme class...
+//     if (document.body.classList.contains("dark-theme")) {
+//         // ...then let's make the theme dark
+//         theme = "dark";
+//     };
+//     // Then save the choice in localStorage
+//     localStorage.setItem("theme", theme);
+// });
+
+// // Listen for a click on the button 
+// btn.addEventListener("touchstart", function() {
+//     console.error('wtf');
+//     // Toggle the .dark-theme class on each click
+//     document.body.classList.toggle("dark-theme");
+
+//     // Let's say the theme is equal to light
+//     let theme = "light";
+//     // If the body contains the .dark-theme class...
+//     if (document.body.classList.contains("dark-theme")) {
+//         // ...then let's make the theme dark
+//         theme = "dark";
+//     };
+//     // Then save the choice in localStorage
+//     localStorage.setItem("theme", theme);
+// });
